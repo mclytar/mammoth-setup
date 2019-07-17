8788f22813e1770c233eb783186db95ce9ecf5d7
Gianluca <gianluca.brian@hotmail.it>
2019-07-18 00:59:21 +0200

    Redefine TODO list in 'lib.rs'

--- src/lib.rs
+++ src/lib.rs
@@ -11,20 +11,21 @@ use std::any::Any;
 
 use toml::Value;
 
+// TODO: complete and apply the `Log` trait to the `MammothInterface` trait.
+// TODO: complete a library version control system.
+
 /// Trait that contains the functions that should be implemented by a module or a handler.
-// TODO: find the best validator type.
 pub trait MammothInterface: Any + Send + Sync {
     /// Function that is called when the library is loaded.
-    fn on_load(&self, _cfg: Option<&Value>) {}
-    // Function that is called during the construction of the server.
-    //
-    // It should output a "factory" function that can be used in `App::configure()`.
-    // TODO: re-doc-comment the above lines.
-    // TODO: load Actix crate.
-    //fn on_factory(&self, _cfg: &mut ServiceConfig) {}
+    fn on_load(&self, _: Option<&Value>) {}
+    // FOR_LATER: load Actix crate and uncomment the following.
+    // /// Function that is called during the construction of the server.
+    // ///
+    // /// It should output a "factory" function that can be used in `App::configure()`.
+    // fn on_factory(&self, _cfg: &mut ServiceConfig) {}
 
-    // TODO: Add Middleware support.
-    // TODO: Add support for interaction between interfaces.
+    // FOR_LATER: Add Middleware support.
+    // FOR_LATER: Add support for interaction between interfaces.
 
     /// Function that is called when the server is shut down.
     fn on_shutdown(&self) {}
